d7a67d44d4a815e092807d0aae14b3e4120c4e85
Lelouching <engenheirogamer123@gmail.com>
2022-10-26 11:42:42 -0300

    feat:Usuários que não fizeram o login, não podem acessar a página home

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,6 +2,17 @@ import { getPosts, createPost } from "../../scripts/api.js"
 import { renderUserInfo, renderPosts } from "../../scripts/render.js";
 import { creatingPost, editPost, deletePost } from "../../scripts/modal.js"
 
+function userIsLogged() {
+    const token = localStorage.getItem("account-token")
+    const tokenParse = JSON.parse(token)
+
+    if(!tokenParse) {
+        window.location.assign("../../index.html")
+    }
+}
+
+userIsLogged()
+
 renderUserInfo()
 
 getPosts(renderPosts)
